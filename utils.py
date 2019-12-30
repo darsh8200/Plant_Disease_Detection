8ffd10f5ecbedbba4256a4d861e487c4653ca76e
darsh8200 <46572301+darsh8200@users.noreply.github.com>
2019-12-30 20:55:50 +0530

    Create utils.py
    It describes different function which felicitates the model function to make the classification.

---
+++ utils.py
@@ -0,0 +1,84 @@
+import numpy as np
+import pickle
+import cv2
+from os import listdir
+from sklearn.preprocessing import LabelBinarizer
+from keras.models import Sequential
+from keras.layers.normalization import BatchNormalization
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.core import Activation, Flatten, Dropout, Dense
+from keras import backend as K
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import Adam
+from keras.preprocessing import image
+from keras.preprocessing.image import img_to_array
+from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+# Values which will be required for model and choosed after optimizing the error.
+EPOCHS = 25
+INIT_LR = 1e-3
+BS = 32
+default_image_size = tuple((256, 256))
+image_size = 0
+directory_root = '../plantvillage/'
+width=256
+height=256
+depth=3
+
+# This function will convert image data to an array format.
+def convert_image_to_array(image_dir):
+    try:
+        image = cv2.imread(image_dir)
+        if image is not None :
+            image = cv2.resize(image, default_image_size)   
+            return img_to_array(image)
+        else :
+            return np.array([])
+    except Exception as e:
+        print(f"Error : {e}")
+        return None
+        
+image_list, label_list = [], []
+try:
+    print("[INFO] Loading images ...")
+    root_dir = listdir(directory_root)
+    for directory in root_dir :
+        # removing .DS_Store from list
+        if directory == ".DS_Store" :
+            root_dir.remove(directory)
+
+    for plant_folder in root_dir :
+        plant_disease_folder_list = listdir(f"{directory_root}/{plant_folder}")
+        
+        for disease_folder in plant_disease_folder_list :
+            # removing .DS_Store from list
+            if disease_folder == ".DS_Store" :
+                plant_disease_folder_list.remove(disease_folder)
+
+        for plant_disease_folder in plant_disease_folder_list:
+            print(f"[INFO] Processing {plant_disease_folder} ...")
+            plant_disease_image_list = listdir(f"{directory_root}/{plant_folder}/{plant_disease_folder}/")
+                
+            for single_plant_disease_image in plant_disease_image_list :
+                if single_plant_disease_image == ".DS_Store" :
+                    plant_disease_image_list.remove(single_plant_disease_image)
+
+            for image in plant_disease_image_list[:200]:
+                image_directory = f"{directory_root}/{plant_folder}/{plant_disease_folder}/{image}"
+                if image_directory.endswith(".jpg") == True or image_directory.endswith(".JPG") == True:
+                    image_list.append(convert_image_to_array(image_directory))
+                    label_list.append(plant_disease_folder)
+    print("[INFO] Image loading completed")  
+except Exception as e:
+    print(f"Error : {e}")
+
+# This will labelize the different 15 classses.
+label_binarizer = LabelBinarizer()
+image_labels = label_binarizer.fit_transform(label_list)
+pickle.dump(label_binarizer,open('label_transform.pkl', 'wb'))
+n_classes = len(label_binarizer.classes_)
+
+
