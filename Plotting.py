227a458f55b955c78eb107121f74ecf95e90f8b3
darsh8200 <46572301+darsh8200@users.noreply.github.com>
2019-12-30 21:16:12 +0530

    Create Plotting.py

---
+++ Plotting.py
@@ -0,0 +1,20 @@
+# These equations extract the data from the model which has completed its training iterations.
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(acc) + 1)
+
+# This plot shows Training and Validation Accuracy.
+plt.plot(epochs, acc, 'b', label='Training accurarcy')
+plt.plot(epochs, val_acc, 'r', label='Validation accurarcy')
+plt.title('Training and Validation accurarcy')
+plt.legend()
+plt.figure()
+
+# This plot shows Training and Validation Loss.
+plt.plot(epochs, loss, 'b', label='Training loss')
+plt.plot(epochs, val_loss, 'r', label='Validation loss')
+plt.title('Training and Validation loss')
+plt.legend()
+plt.show()
